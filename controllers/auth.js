797f2102896f831f76da466591601f93676e268d
RaquelV <raquelv85@gmail.com>
2021-04-08 17:56:50 +0200

    add encryption password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,26 +1,31 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
 
 const crearUsuario = async (req, res = response) => {
   const { email, password } = req.body;
 
   try {
-    let usuario = await Usuario.findOne({email})
+    let usuario = await Usuario.findOne({ email });
 
-    if(usuario){
+    if (usuario) {
       return res.status(400).json({
         ok: false,
-        msg:'Un usuario existe con ese correo'
-      })
+        msg: "Un usuario existe con ese correo",
+      });
     }
     usuario = new Usuario(req.body);
+
+    //Encriptar password
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
 
     await usuario.save();
 
     res.status(201).json({
       ok: true,
       uid: usuario.id,
-      name: usuario.name
+      name: usuario.name,
     });
   } catch (error) {
     res.status(500).json({
