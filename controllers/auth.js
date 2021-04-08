9b3b5d0e269981c7d30b1df5ab3da3ec26c399bf
RaquelV <raquelv85@gmail.com>
2021-04-08 13:53:52 +0200

    add validations to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ const crearUsuario = (req,res = response) => {
     })
   }
 
-  res.json({
+  res.status(201).json({
     ok:true,
     msg:"registro",
     name,
@@ -25,6 +25,16 @@ const crearUsuario = (req,res = response) => {
 
 const loginUsuario = (req,res = response) => {
   const { email,password } = req.body
+
+  //manejo de errores
+  const errors = validationResult( req );
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      ok:false,
+      errors: errors.mapped()
+    })
+  }
 
   res.json({
     ok:true,
