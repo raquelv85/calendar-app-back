34f52db23ae6709d42af2afb26f2018d332dfb36
RaquelV <raquelv85@gmail.com>
2021-04-08 12:37:17 +0200

    create file env

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
+require('dotenv').config();
+
+console.log(process.env)
 
 //Crear server express
 const app = express();
 
+//Directorio público
+//middleware función que se ejecuta en el momento que alguien hace petición al servidor
+app.use( express.static('public') )
+
 // Rutas
-app.get("/", (req,res) =>{
-  res.json({
-    ok:true
-  })
-})
+// app.get("/", (req,res) =>{
+//   res.json({
+//     ok:true
+//   })
+// })
 
 //Escuchar peticiones
-app.listen(4000, () => {
-  console.log(`servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+  console.log(`servidor corriendo en puerto ${process.env.PORT}`)
 })
