e9315238bb01b7bc4fbbc4b4454f1551bcc40d5c
RaquelV <raquelv85@gmail.com>
2021-04-08 13:47:36 +0200

    install express validator and add validations to routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator')
 
 const crearUsuario = (req,res = response) => {
-  const { name, email,password } = req.body
+  const { name, email,password } = req.body;
+
+  //manejo de errores
+  const errors = validationResult( req );
+
+  if(!errors.isEmpty()){
+    return res.status(400).json({
+      ok:false,
+      errors: errors.mapped()
+    })
+  }
 
   res.json({
     ok:true,
--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,19 @@
 */
 
 const {Router} = require('express');
+const { check } = require('express-validator')
 const router = Router();
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth')
 
-router.post("/new", crearUsuario)
+router.post(
+  "/new", 
+  [//middleware
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+  ],
+  crearUsuario)
 
 
 router.post("/", loginUsuario)
