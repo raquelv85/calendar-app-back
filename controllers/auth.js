45b6223556ddf61fe70f0d3fce668a9b87a005c4
RaquelV <raquelv85@gmail.com>
2021-04-08 17:38:43 +0200

    add validation and save user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,26 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator')
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req,res = response) => {
-  const { name, email,password } = req.body;
+const crearUsuario = async(req,res = response) => {
+  //const { name, email,password } = req.body;
 
+  try {
+    const usuario = new Usuario( req.body );
+
+  await usuario.save()
 
   res.status(201).json({
     ok:true,
     msg:"registro",
-    name,
-    email,
-    password
   })
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
+
+  
 }
 
 const loginUsuario = (req,res = response) => {
