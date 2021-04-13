5fa07b0ead8888b82e10d7301ef5e9f567358499
RaquelV <raquelv85@gmail.com>
2021-04-13 13:06:27 +0200

    add validations to delete event

--- controllers/events.js
+++ controllers/events.js
@@ -37,7 +37,7 @@ const actualizarEvento = async (req, res = response) => {
     const evento = await Evento.findById(eventoId);
 
     if (!evento) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: "Evento no existe por ese id",
       });
@@ -58,15 +58,13 @@ const actualizarEvento = async (req, res = response) => {
     const eventoActualizado = await Evento.findByIdAndUpdate(
       eventoId,
       nuevoEvento,
-      {new: true}
+      { new: true }
     );
 
     res.json({
       ok: true,
-      evento: eventoActualizado
+      evento: eventoActualizado,
     });
-
-
   } catch (error) {
     res.status(500).json({
       ok: false,
@@ -75,11 +73,38 @@ const actualizarEvento = async (req, res = response) => {
   }
 };
 
-const eliminarEvento = (req, res = response) => {
-  res.status(201).json({
-    ok: true,
-    msg: "eliminarEvento",
-  });
+const eliminarEvento = async (req, res = response) => {
+  const eventoId = req.params.id;
+  const uid = req.uid;
+
+  try {
+    const evento = await Evento.findById(eventoId);
+
+    if (!evento) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Evento no existe por ese id",
+      });
+    }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegio para eliminar este evento",
+      });
+    }
+
+    await Evento.findByIdAndDelete(eventoId);
+
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador...",
+    });
+  }
 };
 
 module.exports = { getEventos, crearEvento, actualizarEvento, eliminarEvento };
