8637953979ad7702a3b2d0fb2f421478d8885ead
RaquelV <raquelv85@gmail.com>
2021-04-13 10:44:30 +0200

    add endpoints events

--- controllers/events.js
+++ controllers/events.js
@@ -1,69 +1,40 @@
 const { response } = require("express");
 
 
-const getEventos = async (req, res = response) => {
+const getEventos = (req, res = response) => {
 
+  res.status(201).json({
+    ok: true,
+    msg: 'getEventos'
+  });
 
-  try {
-
-    res.status(201).json({
-      ok: true,
-      msg: 'getEventos'
-    });
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      msg: "Por favor hable con el administrador",
-    });
-  }
 };
 
-const crearEvento = async (req, res = response) => {
+const crearEvento = (req, res = response) => {
 
-  try {
+  res.status(201).json({
+    ok: true,
+    msg: 'crearEvento'
+  });
 
-    res.status(201).json({
-      ok: true,
-      msg: 'crearEvento'
-    });
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      msg: "Por favor hable con el administrador",
-    });
-  }
 };
 
-const actualizarEvento = async (req, res = response) => {
+const actualizarEvento = (req, res = response) => {
 
-  try {
+  res.status(201).json({
+    ok: true,
+    msg: 'actualizarEvento'
+  });
 
-    res.status(201).json({
-      ok: true,
-      msg: 'actualizarEvento'
-    });
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      msg: "Por favor hable con el administrador",
-    });
-  }
 };
 
-const eliminarEvento = async (req, res = response) => {
+const eliminarEvento = (req, res = response) => {
 
-  try {
+  res.status(201).json({
+    ok: true,
+    msg: 'eliminarEvento'
+  });
 
-    res.status(201).json({
-      ok: true,
-      msg: 'eliminarEvento'
-    });
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      msg: "Por favor hable con el administrador",
-    });
-  }
 };
 
 
--- routes/events.js
+++ routes/events.js
@@ -5,21 +5,27 @@
 
 const { Router } = require("express");
 const router = Router();
+const { validarJWT } = require('../middlewares/validar-jwt');
 const {
   getEventos,
   crearEvento,
   actualizarEvento,
   eliminarEvento
-} = require("../controllers/auth");
+} = require("../controllers/events");
+
+//Validar token en todos los endpoints para que estén protegidas
+router.use(validarJWT)
 
 // Obtener eventos
 router.get('/', getEventos);
 
 // Crear nuevo evento
-router.post('/new', crearEvento);
+router.post('/', crearEvento);
 
 // Actualizar evento
 router.put('/:id', actualizarEvento);
 
 // Eliminar evento
 router.delete('/:id', eliminarEvento);
+
+module.exports = router;
