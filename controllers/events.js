fea53905865540a4471ed345a282c71f06252204
RaquelV <raquelv85@gmail.com>
2021-04-13 12:56:23 +0200

    add validations to updateEvent

--- controllers/events.js
+++ controllers/events.js
@@ -31,17 +31,41 @@ const crearEvento = async (req, res = response) => {
 
 const actualizarEvento = async (req, res = response) => {
   const eventoId = req.params.id;
+  const uid = req.uid;
 
   try {
-
     const evento = await Evento.findById(eventoId);
 
-    if(!evento){
+    if (!evento) {
       res.status(404).json({
         ok: false,
-        msg: 'Evento no existe por ese id'
-      })
+        msg: "Evento no existe por ese id",
+      });
     }
+
+    if (evento.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegio de editar este evento",
+      });
+    }
+
+    const nuevoEvento = {
+      ...req.body,
+      user: uid,
+    };
+
+    const eventoActualizado = await Evento.findByIdAndUpdate(
+      eventoId,
+      nuevoEvento,
+      {new: true}
+    );
+
+    res.json({
+      ok: true,
+      evento: eventoActualizado
+    });
+
 
   } catch (error) {
     res.status(500).json({
